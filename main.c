e465a1890fab8835e9edb9de836f55e4a6bcc066
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-08-30 21:46:52 +0900

    04

--- main.c
+++ main.c
@@ -4,10 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void){
-	char src[]= "The worst things to eat before you sleep";
-	char dst[100];
-
-strcpy(dst,src);
-	
-	printf("copied string : %s\n", dst);
+	char str[30]= "happy C programming";
+	printf( "string %s length : %i\n", str, strlen(str) );
+	return 0;
 }
+
